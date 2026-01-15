d0bf3f1359dd8a8e84e522ba1e8832384b950e16
Joul <joul1285@gmail.com>
2026-01-15 16:10:35 +0100

    Fix: logger format

--- logger.go
+++ logger.go
@@ -1,17 +1,18 @@
 package worker
 
 import (
+	"fmt"
 	"log"
 	"os"
 )
 
 // Logger interface for worker logging.
 type Logger interface {
-	Debug(args ...interface{})
-	Info(args ...interface{})
-	Warn(args ...interface{})
-	Error(args ...interface{})
-	Fatal(args ...interface{})
+	Debug(format string, args ...interface{})
+	Info(format string, args ...interface{})
+	Warn(format string, args ...interface{})
+	Error(format string, args ...interface{})
+	Fatal(format string, args ...interface{})
 }
 
 // StdLogger is a simple logger using the standard library.
@@ -26,22 +27,22 @@ func NewStdLogger() *StdLogger {
 	}
 }
 
-func (l *StdLogger) Debug(args ...interface{}) {
-	l.logger.Print(append([]interface{}{"[DEBUG] "}, args...)...)
+func (l *StdLogger) Debug(format string, args ...interface{}) {
+	l.logger.Print("[DEBUG] " + fmt.Sprintf(format, args...))
 }
 
-func (l *StdLogger) Info(args ...interface{}) {
-	l.logger.Print(append([]interface{}{"[INFO] "}, args...)...)
+func (l *StdLogger) Info(format string, args ...interface{}) {
+	l.logger.Print("[INFO] " + fmt.Sprintf(format, args...))
 }
 
-func (l *StdLogger) Warn(args ...interface{}) {
-	l.logger.Print(append([]interface{}{"[WARN] "}, args...)...)
+func (l *StdLogger) Warn(format string, args ...interface{}) {
+	l.logger.Print("[WARN] " + fmt.Sprintf(format, args...))
 }
 
-func (l *StdLogger) Error(args ...interface{}) {
-	l.logger.Print(append([]interface{}{"[ERROR] "}, args...)...)
+func (l *StdLogger) Error(format string, args ...interface{}) {
+	l.logger.Print("[ERROR] " + fmt.Sprintf(format, args...))
 }
 
-func (l *StdLogger) Fatal(args ...interface{}) {
-	l.logger.Fatal(append([]interface{}{"[FATAL] "}, args...)...)
+func (l *StdLogger) Fatal(format string, args ...interface{}) {
+	l.logger.Fatal("[FATAL] " + fmt.Sprintf(format, args...))
 }
